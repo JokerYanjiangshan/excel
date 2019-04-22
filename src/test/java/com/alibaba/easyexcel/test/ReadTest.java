1a8be97fc0f72a2cac69a48449e771951217f6ed
JokerYanjiangshan <https://github.com/JokerYanjiangshan/springlcn.git>
2019-04-22 11:56:17 +0800

    地方

--- src/test/java/com/alibaba/easyexcel/test/ReadTest.java
+++ src/test/java/com/alibaba/easyexcel/test/ReadTest.java
@@ -208,4 +208,12 @@ public class ReadTest {
             System.out.println(ob);
         }
     }
+
+    public void woriri(List<Object> datas){
+        int i=0;
+        for (Object ob:datas) {
+            System.out.println(i++);
+            System.out.println(ob);
+        }
+    }
 }
